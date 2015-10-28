a29740ff71ac75c759c7c9e7d7c6e82c8caeb4d1
John Gerrard Holland <office@jgholland.de>
2015-10-28 08:27:44 +0100

    extract file utilities into a new file

--- src/de/jgholland/directorymesh/testutil/InitFileSystem.java
+++
@@ -1,9 +0,0 @@
-package de.jgholland.directorymesh.testutil;
-
-/**
- * Created by john on 2015-10-26.
- */
-public class InitFileSystem {
-
-
-}
--- tests/de/jgholland/directorymesh/MainTest.java
+++ tests/de/jgholland/directorymesh/MainTest.java
@@ -1,5 +1,6 @@
 package de.jgholland.directorymesh;
 
+import de.jgholland.directorymesh.operations.FileUtilities;
 import org.junit.*;
 
 import java.io.File;
@@ -11,89 +12,41 @@ import java.nio.file.*;
 public class MainTest {
 
     Path temporaryDirectoryPath;
-    Path masterDirectoryPath;
-    Path dataDirectoryPath;
     String temporaryDirectoryPathString;
-    String masterDirectoryPathString;
-    String dataDirectoryPathString;
+    FileUtilities fileUtilities;
 
     @org.junit.Before
     public void setUp() throws Exception {
         temporaryDirectoryPath = java.nio.file.Files.createTempDirectory("directoryMesh");
-        masterDirectoryPath = Paths.get(temporaryDirectoryPath.toString(), "master/");
-        dataDirectoryPath = Paths.get(temporaryDirectoryPath.toString(), "data/");
+        Path masterDirectoryPath = Paths.get(temporaryDirectoryPath.toString(), "master/");
+        Path dataDirectoryPath = Paths.get(temporaryDirectoryPath.toString(), "data/");
+        fileUtilities = new FileUtilities(masterDirectoryPath, dataDirectoryPath);
         temporaryDirectoryPathString = temporaryDirectoryPath.toString();
-        masterDirectoryPathString = masterDirectoryPath.toString();
-        dataDirectoryPathString = dataDirectoryPath.toString();
         System.out.println("created " + masterDirectoryPath + "\n" + dataDirectoryPath);
     }
 
     @org.junit.After
     public void tearDown() throws Exception {
-        org.apache.commons.io.FileUtils.deleteDirectory(new File(masterDirectoryPath.toString()));
-        org.apache.commons.io.FileUtils.deleteDirectory(new File(dataDirectoryPath.toString()));
-        System.out.println("deleted " + masterDirectoryPath + "\n" + dataDirectoryPath);
-    }
-
-    public Path pathInMasterDirectory(String path) {
-        return Paths.get(masterDirectoryPath + "/" + path);
-    }
-
-    public Path pathInDataDirectory(String path) {
-        return Paths.get(dataDirectoryPath + "/" + path);
-    }
-
-    public void touchFile(Path path) throws Exception {
-        createDirectory(path.getParent());
-        Files.createFile(path);
-        fileCreationReporter("Created regular file " + path);
-    }
-
-
-    public void touchLink(Path regularFileLocation, Path softLinkLocation) throws Exception {
-        touchFile(regularFileLocation);
-        Files.createSymbolicLink(softLinkLocation, regularFileLocation);
-        reportNewSoftLink(regularFileLocation, softLinkLocation);
-    }
-
-    public void createDirectory(Path path) throws Exception {
-        if (!Files.isDirectory(path)) {
-            Files.createDirectories(path);
-            fileCreationReporter("Created directory " + path);
-        }
-    }
-
-    public void createLinkedDirectory(Path regularDirectoryLocation, Path softLinkLocation) throws Exception {
-        createDirectory(regularDirectoryLocation);
-        Files.createSymbolicLink(softLinkLocation, regularDirectoryLocation);
-        reportNewSoftLink(regularDirectoryLocation, softLinkLocation);
-    }
-
-    public void fileCreationReporter(String string) {
-        String newstring = string.replaceAll(temporaryDirectoryPathString, "");
-        System.out.println(newstring);
-    }
-
-    public void reportNewSoftLink(Path regularLocation, Path softLinkLocation) {
-        fileCreationReporter("Created symbolic link " + regularLocation.toString() + " <- " + softLinkLocation.toString());
+        org.apache.commons.io.FileUtils.deleteDirectory(new File(temporaryDirectoryPathString));
+        System.out.println("deleted " + temporaryDirectoryPathString);
     }
 
     @Test
     public void testFilesystemCreation() throws Exception {
-        touchFile(pathInDataDirectory("datafile0"));
-        touchFile(pathInDataDirectory("datafile1"));
-        touchFile(pathInDataDirectory("commonDir/datafile2"));
-        createDirectory(pathInMasterDirectory("commonDir/"));
-        createDirectory(pathInMasterDirectory("dirOnlyInMaster/datafile3"));
-        touchLink(pathInDataDirectory("datafile4"), pathInMasterDirectory("datafile4"));
+        fileUtilities.touchFile(fileUtilities.pathInDataDirectory("datafile0"));
+        fileUtilities.touchFile(fileUtilities.pathInDataDirectory("datafile1"));
+        fileUtilities.touchFile(fileUtilities.pathInDataDirectory("commonDir/datafile2"));
+        fileUtilities.createDirectory(fileUtilities.pathInMasterDirectory("commonDir/"));
+        fileUtilities.createDirectory(fileUtilities.pathInMasterDirectory("dirOnlyInMaster/datafile3"));
+        fileUtilities.touchWithLink(fileUtilities.pathInDataDirectory("datafile4"), fileUtilities.pathInMasterDirectory("datafile4"));
     }
 
     @Test
     public void testMain() throws Exception {
         Main mainNQ = new de.jgholland.directorymesh.Main();
-        mainNQ.main(new String[] { "-m", masterDirectoryPathString, "-d", dataDirectoryPathString, "-nq"});
-        de.jgholland.directorymesh.Main.main(new String[] {"-q",  "-m", masterDirectoryPathString, "-d", dataDirectoryPathString});
-        de.jgholland.directorymesh.Main.main(new String[] {"-m", masterDirectoryPathString, "-d", dataDirectoryPathString, "-n"});
+        mainNQ.main(new String[] { "-m", fileUtilities.masterDirectoryPathString, "-d", fileUtilities.dataDirectoryPathString, "-nq"});
+        de.jgholland.directorymesh.Main.main(new String[] {"-q",  "-m", fileUtilities.masterDirectoryPathString, "-d", fileUtilities.dataDirectoryPathString});
+        de.jgholland.directorymesh.Main.main(new String[] {"-m", fileUtilities.masterDirectoryPathString, "-d", fileUtilities.dataDirectoryPathString, "-n"});
 
     }
 }
